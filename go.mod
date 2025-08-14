021b56bfe34fe11b34a33aa92508614065eb8b8e
nitikhon <140893068+nitikhon@users.noreply.github.com>
2025-08-14 14:05:09 +0700

    [feat] add mock implementations for ItemRepository and item unit tests

--- go.mod
+++ go.mod
@@ -4,8 +4,10 @@ go 1.23.5
 
 require (
 	github.com/andybalholm/brotli v1.1.0 // indirect
+	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/gofiber/fiber/v2 v2.52.6 // indirect
 	github.com/golang-jwt/jwt/v4 v4.5.2 // indirect
+	github.com/golang/mock v1.6.0 // indirect
 	github.com/google/uuid v1.6.0 // indirect
 	github.com/jackc/pgpassfile v1.0.0 // indirect
 	github.com/jackc/pgservicefile v0.0.0-20221227161230-091c0ba34f0a // indirect
@@ -18,7 +20,9 @@ require (
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/mattn/go-runewidth v0.0.16 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/rivo/uniseg v0.2.0 // indirect
+	github.com/stretchr/testify v1.10.0 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasthttp v1.51.0 // indirect
 	github.com/valyala/tcplisten v1.0.0 // indirect
@@ -26,6 +30,7 @@ require (
 	golang.org/x/sync v0.12.0 // indirect
 	golang.org/x/sys v0.31.0 // indirect
 	golang.org/x/text v0.23.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 	gorm.io/driver/postgres v1.5.11 // indirect
 	gorm.io/gorm v1.25.12 // indirect
 )
---
+++ internal/core/service/item_service_test.go
@@ -0,0 +1,257 @@
+package service
+
+import (
+	"errors"
+	"fmt"
+	"testing"
+
+	"github.com/golang/mock/gomock"
+	"github.com/nitikhon/golang-inventory-system/internal/core/entity"
+	mock_port "github.com/nitikhon/golang-inventory-system/internal/core/port/mock"
+	"github.com/stretchr/testify/assert"
+	"gorm.io/gorm"
+)
+
+func TestNewItemService(t *testing.T) {
+	// arrange
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	mockItemRepo := mock_port.NewMockItemRepository(ctrl)
+
+	// act
+	mockItemService := NewItemService(mockItemRepo)
+
+	// assert
+	assert.NotNil(t, mockItemService)
+}
+
+func TestGetAllItems(t *testing.T) {
+	// arrange
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	mockItemRepo := mock_port.NewMockItemRepository(ctrl)
+	mockItemRepo.EXPECT().GetAllItems().Return([]*entity.Item{}, nil)
+
+	// act
+	itemService := NewItemService(mockItemRepo)
+	items, err := itemService.GetAllItems()
+
+	// assert
+	assert.NotNil(t, items, fmt.Sprintf("expect empty array, got %v", items))
+	assert.Nil(t, err, fmt.Sprintf("expect nil, got %v", err))
+}
+
+func TestGetItemById(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	mockItemRepo := mock_port.NewMockItemRepository(ctrl)
+
+	tests := []struct {
+		name       string
+		id         uint
+		mockReturn *entity.Item
+		mockErr    error
+		expectErr  bool
+	}{
+		{
+			name:       "success: found item",
+			id:         1,
+			mockReturn: &entity.Item{Model: gorm.Model{ID: 1}},
+			mockErr:    nil,
+			expectErr:  false,
+		},
+		{
+			name:       "fail: not found",
+			id:         999,
+			mockReturn: &entity.Item{},
+			mockErr:    fmt.Errorf("item not found"),
+			expectErr:  true,
+		},
+		{
+			name:       "fail: db error",
+			id:         2,
+			mockReturn: &entity.Item{},
+			mockErr:    fmt.Errorf("db error"),
+			expectErr:  true,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			// arrange
+			mockItemRepo.
+				EXPECT().
+				GetItemByID(tt.id).
+				Return(tt.mockReturn, tt.mockErr)
+
+			// act
+			itemService := NewItemService(mockItemRepo)
+			item, err := itemService.GetItemByID(tt.id)
+
+			// assert
+			if tt.expectErr {
+				assert.NotNil(t, err, "expect error to be not nil")
+			} else {
+				assert.Nil(t, err, fmt.Sprintf("expect error to be nil, got %v", err))
+			}
+
+			assert.Equal(t, tt.mockReturn, item, "expect an item to be %v, got %v", tt.mockReturn, item)
+		})
+	}
+}
+
+func TestCreate(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	mockItemRepo := mock_port.NewMockItemRepository(ctrl)
+
+	tests := []struct {
+		name            string
+		itemName        string
+		AvailableAmount int
+		mockReturn      *entity.Item
+		mockErr         error
+		expectErr       bool
+	}{
+		{
+			name:            "success: create an item",
+			itemName:        "test",
+			AvailableAmount: 10,
+			mockReturn:      &entity.Item{Name: "test", AvailableAmount: 10, TotalAmount: 10},
+			mockErr:         nil,
+			expectErr:       false,
+		},
+		{
+			name:       "failed: empty item name",
+			itemName:   "",
+			mockReturn: &entity.Item{},
+			mockErr:    errors.New("name is required"),
+			expectErr:  true,
+		},
+		{
+			name:            "failed: negative AvailableAmount",
+			itemName:        "test",
+			AvailableAmount: -1,
+			mockReturn:      &entity.Item{},
+			mockErr:         errors.New("AvailableAmount cannot be negative"),
+			expectErr:       true,
+		},
+		{
+			name:            "failed: zero AvailableAmount",
+			itemName:        "test",
+			AvailableAmount: 0,
+			mockReturn:      &entity.Item{},
+			mockErr:         errors.New("AvailableAmount cannot be zero"),
+			expectErr:       true,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			// arrange
+			if !tt.expectErr {
+				mockItemRepo.
+					EXPECT().
+					Create(gomock.Any()).
+					Return(tt.mockReturn, tt.mockErr)
+			}
+
+			// act
+			itemService := NewItemService(mockItemRepo)
+			item, err := itemService.Create(&entity.Item{
+				Name: tt.itemName, 
+				AvailableAmount: tt.AvailableAmount})
+
+			// assert
+			if tt.expectErr {
+				assert.NotNil(t, err, "expect an error, got nil")
+			} else {
+				assert.Nil(t, err, "expect nil, got %v", err)
+			}
+
+			assert.Equal(t, tt.mockReturn, item, "expect %v, got %v", tt.mockReturn, item)
+		})
+	}
+}
+
+func TestUpdate(t *testing.T) {
+	// arrange
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	mockItemRepo := mock_port.NewMockItemRepository(ctrl)
+	mockItemRepo.EXPECT().Update(gomock.Any()).Return(&entity.Item{Name: "updatedName"}, nil)
+
+	// act
+	ItemService := NewItemService(mockItemRepo)
+	item, err := ItemService.Update(&entity.Item{Name: "updatedName"})
+
+	// assert
+	assert.NotNil(t, item, "expect an item, got nil")
+	assert.Nil(t, err, fmt.Sprintf("expect an error to be nil, got %v", err))
+	assert.Equal(t, "updatedName", item.Name)
+}
+
+func TestDelete(t *testing.T) {
+	// arrange
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	mockItemRepo := mock_port.NewMockItemRepository(ctrl)
+	mockItemRepo.EXPECT().Delete(gomock.Any()).Return(nil)
+
+	// act
+	ItemService := NewItemService(mockItemRepo)
+	err := ItemService.Delete(uint(1))
+
+	// assert
+	assert.Nil(t, err, fmt.Sprintf("expect an error to be nil, got %v", err))
+}
+
+func TestGetItemByIdForUpdate(t *testing.T) {
+	// arrange
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	mockItemRepo := mock_port.NewMockItemRepository(ctrl)
+	mockItemRepo.
+		EXPECT().
+		GetItemByIDForUpdate(gomock.Any(), gomock.Any()).
+		Return(&entity.Item{Model: gorm.Model{ID: 1}}, nil)
+	tx := &gorm.DB{}
+
+	// act
+	ItemService := NewItemService(mockItemRepo)
+	item, err := ItemService.GetItemByIDForUpdate(tx, uint(1))
+
+	// assert
+	assert.Nil(t, err, fmt.Sprintf("expect an error to be nil, got %v", err))
+	assert.NotNil(t, item)
+	assert.Equal(t, uint(1), item.ID)
+}
+
+func TestUpdateWithTx(t *testing.T) {
+	// arrange
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	mockItemRepo := mock_port.NewMockItemRepository(ctrl)
+	mockItemRepo.
+		EXPECT().
+		UpdateWithTx(gomock.Any(), gomock.Any()).
+		Return(&entity.Item{Name: "updatedName"}, nil)
+	tx := &gorm.DB{}
+	
+	// act
+	ItemService := NewItemService(mockItemRepo)
+	item, err := ItemService.UpdateWithTx(tx, &entity.Item{Name: "updatedName"})
+
+	// assert
+	assert.Nil(t, err, fmt.Sprintf("expect an error to be nil, got %v", err))
+	assert.NotNil(t, item)
+	assert.Equal(t, "updatedName", item.Name)
+}
