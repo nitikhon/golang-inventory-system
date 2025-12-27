7550aedd2f99b00ff9ec63865d49b77feb347fad
nitikhon <140893068+nitikhon@users.noreply.github.com>
2025-12-27 17:14:20 +0700

    [refactor] remove ; in item.ts

--- frontend/src/services/item.ts
+++ frontend/src/services/item.ts
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const isDev = import.meta.env.DEV; 
+const isDev = import.meta.env.DEV 
 const baseUrl = isDev ? 'http://localhost:3000/items' : '/api/items'
 
 const getAll = async () => {
