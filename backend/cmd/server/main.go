2395cf7d09dae1fc227d8edf2c6218d399b2af54
nitikhon <140893068+nitikhon@users.noreply.github.com>
2026-01-15 16:09:55 +0700

    [fix] add patch method to cors config to handle patch update

--- backend/cmd/server/main.go
+++ backend/cmd/server/main.go
@@ -57,7 +57,7 @@ func main() {
 	app.Use(cors.New(cors.Config{
 		AllowOrigins:     "http://localhost:5173",
 		AllowHeaders:     "Origin, Content-Type, Accept, Authorization",
-		AllowMethods:     "GET, POST, PUT, DELETE, OPTIONS",
+		AllowMethods:     "GET, POST, PUT, DELETE, OPTIONS, PATCH",
 		AllowCredentials: true,
 	}))
 
